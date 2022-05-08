66f2ba5360c65c7206c6bb3408eab8f10d47699c
Hafizul Islam <hafizulislam3120@gmail.com>
2022-05-08 00:51:06 -0700

    learned javascript

---
+++ arrayone.js
@@ -0,0 +1,11 @@
+const ProgrammingLanguage = ['javascript','java','c','C++','python','PHP'];
+// console.log(ProgrammingLanguage[0]);
+// console.log(ProgrammingLanguage[1]);
+// console.log(ProgrammingLanguage[2]);
+// console.log(ProgrammingLanguage[3]);
+// console.log(ProgrammingLanguage[4]);
+// console.log(ProgrammingLanguage[5]);
+// console.log(ProgrammingLanguage);
+for(i=0; i<=5;i++){
+    console.log(ProgrammingLanguage[i]);
+}
